9af99bc103362459c9720aef3e82fb0288694571
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-16 15:51:41 -0700

    work on update entry, does not work

--- src/views/Entry.jsx
+++ src/views/Entry.jsx
@@ -8,11 +8,49 @@ import styles from './Entry.css';
 export default function Entry() {
   const { id } = useParams();
   const [entry, setEntry] = useState({});
-  const { entries, getEntry } = useEntries();
+  const { entries, getEntry, updateEntry } = useEntries();
+  const [isEditing, setIsEditing] = useState(false);
+
+  let content;
 
   useEffect(() => {
     setEntry(getEntry(id));
   }, [id, entries.length]);
+
+  if (isEditing) {
+    content = (
+      <form onSubmit={(e) => { e.preventDefault(); setIsEditing(false); }} className={styles.form}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Plan something"
+          value={entry.title}
+          onChange={(e) => updateEntry({ ...entry, title: e.target.value })}
+          className={styles.input}
+        />
+        {/* <input
+          type="date"
+          name="date"
+          aria-label="Due date"
+          value={entry.date || ''}
+          onChange={(e) => updateEntry({ ...entry, date: e.target.value })}
+          className={styles.input}
+        /> */}
+        <textarea
+          name="content"
+          placeholder="A brief description of what you're planning"
+          value={entry.content}
+          onChange={(e) => updateEntry({ ...entry, content: e.target.value })}
+          className={styles.content}
+        />
+        <button type="submit edits">Save Edits</button>
+      </form>
+    )
+  } else {
+    content = (
+      <button type='button' aria-label='edit button' onClick={() => setIsEditing(true)}>Edit</button>
+    )
+  }
 
   return (
     <>
@@ -24,6 +62,7 @@ export default function Entry() {
         <p>Due: {entry?.date}</p>
         <p>{entry?.content}</p>
       </article>
+      {content} 
     </>
   );
 }
